6319ac9a5c0b7714e6aeeb9e252ff8d140ab2dd9
Dharmik-kachariya <prajapatidk525@gmail.com>
2025-06-30 16:50:43 +0530

    complate Uppercase in textUtils

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 
 export default function TextForms(prop) {
+  
+  const handleUpClick = () =>{
+    setText("You have click handleUpClick.")
+    let newText = text.toUpperCase();
+    setText(newText)
+  } 
+
+    const handleOnChange = (event) =>{
+    setText(event.target.value);
+  }
+
   const [text, setText] = useState("Enter text here");
   return (
     <div>
       <div>
-        <h2>{prop.heading}</h2>
-        <textarea className="form-control" id="myBox" rows="8"></textarea>
+        <h2>{prop.heading}-</h2>
+        <textarea className="form-control" onChange={handleOnChange} id="myBox" value ={text} rows="8"></textarea>
       </div>
-      <button className="btn btn-primary">Convert to upperCase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to upperCase</button>
     </div>
   );
 }
