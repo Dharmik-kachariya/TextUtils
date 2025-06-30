327bb2d05db0b9e400406fa42ac5c74ee31f6339
Dharmik-kachariya <prajapatidk525@gmail.com>
2025-06-30 17:15:32 +0530

    Complate Summary and Text Preview in textUtils

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,25 +1,40 @@
 import React, { useState } from "react";
 
 export default function TextForms(prop) {
-  
-  const handleUpClick = () =>{
-    setText("You have click handleUpClick.")
+  const handleUpClick = () => {
+    setText("You have click handleUpClick.");
     let newText = text.toUpperCase();
-    setText(newText)
-  } 
+    setText(newText);
+  };
 
-    const handleOnChange = (event) =>{
+  const handleOnChange = (event) => {
     setText(event.target.value);
-  }
+  };
 
   const [text, setText] = useState("Enter text here");
   return (
-    <div>
-      <div>
+    <>
+      <div className="container my-3">
         <h2>{prop.heading}-</h2>
-        <textarea className="form-control" onChange={handleOnChange} id="myBox" value ={text} rows="8"></textarea>
+        <textarea
+          className="form-control"
+          onChange={handleOnChange}
+          id="myBox"
+          value={text}
+          rows="8"
+        ></textarea>
+
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to upperCase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to upperCase</button>
-    </div>
+      <div className="container my-3">
+        <h3>Your Text summary</h3>
+        <p>{text.split("").length} words and {text.length} characters.</p>
+        <p>{0.008 * text.split("").length} minutes read.</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
